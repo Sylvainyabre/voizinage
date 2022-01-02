fffbc577fa5e847b6013371b5c822b7b288a70f7
Sylvainyabre <sylvainyabre7@gmail.com>
2022-01-02 00:56:10 -0800

    first commit

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+    
       <Text>Welcome to Voizinage</Text>
       <StatusBar style="auto" />
     </View>
